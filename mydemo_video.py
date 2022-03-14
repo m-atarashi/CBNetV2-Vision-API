5f7824a580df204b9e613df4d2e13be8751863c7
m-atarashi <m-atarashi@sumilab.org>
2022-03-14 20:38:05 +0900

    fix

--- mydemo_video.py
+++ mydemo_video.py
@@ -25,7 +25,7 @@ def demo(img, model, score_thr=0.3):
 def main():
     model_index = 1
     # init a detector
-    model = init_detector(config_files[model_index], checkpoint_files[model_index], device=device)
+    model = init_detector(configss[model_index], checkpoinss[model_index], device=device)
     
     imgs = sorted(glob('demo/input_frames/*.png'))
     for img in imgs:
