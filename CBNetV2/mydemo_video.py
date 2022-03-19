2706228bd9cce8089dbb9d353a9a00f4ba675a08
m-atarashi <m-atarashi@sumilab.org>
2022-03-20 00:21:10 +0900

    fix

--- CBNetV2/mydemo_video.py
+++ CBNetV2/mydemo_video.py
@@ -4,13 +4,15 @@ from glob import glob
 import ffmpeg
 
 
+CBNetV2_HOME = '/home/m-atarashi/CBNetV2/CBNetV2/'
+
 configs = [
-    'configs/cbnet/htc_cbv2_swin_base_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_20e_coco.py',
-    'configs/cbnet/htc_cbv2_swin_large_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_1x_coco.py'
+    f'{CBNetV2_HOME}configs/cbnet/htc_cbv2_swin_base_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_20e_coco.py',
+    f'{CBNetV2_HOME}configs/cbnet/htc_cbv2_swin_large_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_1x_coco.py'
 ]
 checkpoints = [
-    'checkpoints/htc_cbv2_swin_base22k_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_20e_coco.pth',
-    'checkpoints/htc_cbv2_swin_large22k_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_1x_coco.pth'
+    f'{CBNetV2_HOME}checkpoints/htc_cbv2_swin_base22k_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_20e_coco.pth',
+    f'{CBNetV2_HOME}checkpoints/htc_cbv2_swin_large22k_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_1x_coco.pth'
 ]
 
 device = 'cuda:0'
@@ -19,7 +21,7 @@ device = 'cuda:0'
 def demo(img, model, score_thr=0.3):
     # inference the demo image
     result = inference_detector(model, img)
-    output = 'demo/output_frames/' + splitext(basename(img))[0] + '_output.jpg'
+    output = '../demo_data/outputs/output_frames/' + splitext(basename(img))[0] + '_output.jpg'
     model.show_result(img, result, score_thr=score_thr, out_file=output)
 
 
@@ -28,7 +30,7 @@ def main():
     # init a detector
     model = init_detector(configs[model_index], checkpoints[model_index], device=device)
     
-    imgs = sorted(glob('demo/input_frames/*.png'))
+    imgs = sorted(glob('../demo_data/outputs/input_frames/*.png'))
     for img in imgs:
         demo(img, model)
 
--- ffmpeg_demo/test2.py
+++ ffmpeg_demo/test2.py
@@ -1,12 +1,12 @@
 import ffmpeg
 import numpy as np
 from PIL import Image
+from CBNetV2 import mydemo
 
 input_path = '../demo_data/inputs/shelving_short.mp4'
 
 height = ffmpeg.probe(input_path)['streams'][0]['height']
 width = ffmpeg.probe(input_path)['streams'][0]['width']
-
 
 output = (
     ffmpeg
@@ -17,7 +17,3 @@ output = (
 
 frames = np.frombuffer(output, np.uint8).reshape([-1, height, width, 3])
 
-print(frames.shape)
-Image.fromarray(frames[100]).save('../demo_data/outputs/test2_1.jpg')
-Image.fromarray(frames[200]).save('../demo_data/outputs/test2_2.jpg')
-Image.fromarray(frames[300]).save('../demo_data/outputs/test2_3.jpg')
