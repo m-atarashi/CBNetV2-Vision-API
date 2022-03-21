02ffa68c108933a0cdff4fa3e4ac50d02d45796e
m-atarashi <m-atarashi@sumilab.org>
2022-03-21 19:45:41 +0900

    add: plain text image

--- utils/paddle_ocr_demo.sh
+++ utils/paddle_ocr_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python ../PaddleOCR/tools/infer/predict_system.py \
---image_dir="../data/inputs/book_0020.jpg" \
+--image_dir="../data/inputs/plain_text_01.png" \
 --det_model_dir="../PaddleOCR/checkpoints/ch_ppocr_server_v2.0_det_infer" \
 --cls_model_dir="../PaddleOCR/checkpoints/ch_ppocr_mobile_v2.0_cls_infer" \
 --rec_model_dir="../PaddleOCR/checkpoints/japan_mobile_v2.0_rec_infer" \
