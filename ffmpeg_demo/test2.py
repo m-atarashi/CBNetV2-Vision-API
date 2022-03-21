b1963e4de0fa1a8177222218dfe5d8eec968e659
m-atarashi <m-atarashi@sumilab.org>
2022-03-21 16:16:27 +0900

    fix

--- ffmpeg_demo/test2.py
+++ ffmpeg_demo/test2.py
@@ -7,13 +7,11 @@ import numpy as np
 from PIL import Image
 from numba import jit
 
-from CBNetV2 import mydemo, mydemo_batch, mydemo_batch_alter
+from CBNetV2 import mydemo
 
 
 video_path = '../data/inputs/shelving_short_02.mp4'
 output_root = '../data/outputs'
-# batch_size over 2 causes CUDA out of memory lol
-inference_batch_size = 2
 
 
 def extract_frames(video_path):
@@ -33,19 +31,27 @@ def extract_frames(video_path):
 
 def main():
     frames = extract_frames(video_path)
-    model = mydemo_batch.load_model()
+    model = mydemo.load_model()
 
-    step = inference_batch_size
-    for slice_start in range(0, len(frames), step):
-        print(f'frames: {slice_start}~{slice_start+step-1}')
-        batch_frames = frames[slice_start : slice_start + step]
+    for i, frame in enumerate(frames):
+        print(f'frame: {i+1}')
 
-        results = mydemo_batch.inference(batch_frames, model)
-        for i, result in enumerate(results):
-            output_dir = f'{output_root}/{splitext(basename(video_path))[0]}/frame_{str(i + slice_start).zfill(8)}/'
-            makedirs(output_dir, exist_ok = True)
+        result = mydemo.inference(frame, model)
 
-            mydemo_batch.save_instances(batch_frames[i], result, score_thr=0.3, output_dir=output_dir)
+        output_dir = f'{output_root}/{splitext(basename(video_path))[0]}/frame_{str(i + slice_start).zfill(8)}/'
+        makedirs(output_dir, exist_ok = True)
+        mydemo.save_instances(frame, result, score_thr=0.3, output_dir=output_dir)
+
+
+if __name__ == '__main__':
+    t_s = time.time()
+    main()
+    t_e = time.time()
+    print(f'Processing time: {float(t_e-t_s)}[s]')
+
+
+
+
 
 
 # 徒労
@@ -74,9 +80,3 @@ def main():
 
 #             mydemo_batch_alter.save_instances(batch_frames[i], coords, scores, masks, score_thr=0.3, output_dir=output_dir)
 
-
-if __name__ == '__main__':
-    t_s = time.time()
-    main()
-    t_e = time.time()
-    print(f'Processing time: {float(t_e-t_s)}[s]')
